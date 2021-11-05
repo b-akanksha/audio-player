5c9632492b468da6b9320a20cb20a6db57922091
b-akanksha <b.akanksha6@gmail.com>
2021-11-05 14:42:52 +0530

    Initial Setup

---
+++ src/tracks.js
@@ -0,0 +1,25 @@
+const tracks = [
+  {
+    title: "Run Away",
+    artist: "EXO",
+    audioSrc: "./assets/music/EXO-Runaway.mp3",
+    image: "./assets/images/EXO-Runaway.jpeg",
+    color: "#00aeb0",
+  },
+  {
+    title: "Run Away",
+    artist: "GOT7",
+    audioSrc: "./assets/music/GOT7-Runaway.mp3",
+    image: "./assets/images/GOT7-Runaway.jpeg",
+    color: "#ffb77a",
+  },
+  {
+    title: "Run Away",
+    artist: "Eric Nam",
+    audioSrc: "./assets/music/Eric-Runaway.mp3",
+    image: "./assets/images/Eric-Runaway.jpg",
+    color: "#5f9fff",
+  },
+];
+
+export default tracks;
